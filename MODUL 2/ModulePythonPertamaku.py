b2230a5d880643780bacd467467030489ffe2aa7
L200180005 <48120097+L200180005@users.noreply.github.com>
2020-03-20 22:19:27 +0700

    Add files via upload

---
+++ MODUL 2/ModulePythonPertamaku.py
@@ -0,0 +1,7 @@
+def ucapkanSalam():
+    print ("assalamu'alaikum")
+def kuadratkan(x):
+    return x*x
+buah = 'mangga'
+daftarbaju = ['batik', 'resmi', 'resmi berdasi']
+jumlahbaju = len(daftarbaju)
---
+++ MODUL 2/lat2.5.py
@@ -0,0 +1,5 @@
+import lat3
+class MhsTIF(Mahasiswa):
+    """ Class MhsTIF yang di bangun dari class Mahasiswa"""
+    def katakanPy(self):
+        print('Python is cool.')
